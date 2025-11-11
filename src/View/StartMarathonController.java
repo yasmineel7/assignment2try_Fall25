879569a7b7081ff49610043ccaf3a5cdcb476c62
yasmineel7 <6298686@edu.vaniercollege.qc.ca>
2025-11-11 16:14:26 -0500

    creation of a scene for the beguinning of the race and it's controller

---
+++ src/View/StartMarathonController.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package View;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author 6298686
+ */
+public class StartMarathonController implements Initializable {
+
+    @FXML
+    private Button startButton;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void switchToScene1(ActionEvent event) {
+    }
+    
+}
