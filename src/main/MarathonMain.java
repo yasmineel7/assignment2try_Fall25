20ad9195c2b78fe8db491657114b164e1d7c24f9
yasmineel7 <6298686@edu.vaniercollege.qc.ca>
2025-11-11 14:31:07 -0500

    the main

---
+++ src/main/MarathonMain.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MarathonMain extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/marathon.fxml"));
+        Scene scene = new Scene(loader.load());
+        primaryStage.setTitle("Marathon Simulator");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
