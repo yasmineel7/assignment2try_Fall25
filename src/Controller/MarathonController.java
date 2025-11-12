58d7fe131649be038060f366648993d837dc50a5
yasmineel7 <6298686@edu.vaniercollege.qc.ca>
2025-11-12 17:44:52 -0500

    added a few images and rectangles to advance the output

--- src/Controller/MarathonController.java
+++ src/Controller/MarathonController.java
@@ -6,17 +6,25 @@ package Controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Line;
+import javafx.scene.shape.Polyline;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 /**
  * FXML Controller class
@@ -54,6 +62,64 @@ import javafx.stage.Stage;
 
     @FXML
     private Line thirdLine;
+    
+     @FXML
+    private ImageView endRace;
+     
+     @FXML
+     private ImageView runnerMoving;
+     
+     //other variables
+     Image[] images = new Image[2];
+     Timeline animation;
+     Timeline runCycleAnimation;
+     int index = 0;
+     double startX;
+     
+     @FXML
+     void runnerMoving(ActionEvent event) {
+         int num = 1;
+        for (int i = 0; i < images.length; i++) {
+            images[i] = new Image("file:images//runner" + (num + 1) + ".png"); //right code??
+            runnerMoving = new ImageView(images[i]);
+            KeyFrame frame1 = new KeyFrame(Duration.seconds(5), new KeyValue(runnerMoving.layoutXProperty(), 70));
+           KeyFrame frame2 = new KeyFrame(Duration.seconds(2), new KeyValue(runnerMoving.layoutXProperty(), 200));
+            animation = new Timeline(frame1, frame2);
+           
+      
+        animation.play();
+        }
+//         if (animation != null) {
+//             animation.stop();
+//         }
+//         if (runCycleAnimation != null) {
+//             runCycleAnimation.stop();
+//         }
+//         
+//         
+//         double endX = 500.0;
+//         
+//         KeyValue keyValue = new KeyValue(runnerMoving.layoutXProperty(), endX);
+//         KeyFrame keyframe = new KeyFrame(Duration.seconds(10), keyValue);
+//         
+//         animation = new Timeline(keyframe);
+//         animation.setCycleCount(1);
+//         
+//         KeyFrame imageKeyFrame = new KeyFrame(Duration.seconds(0.1), e -> {
+//             index = (index + 1) % images.length;
+//             runnerMoving.setImage(images[index]);
+//         });
+//         
+//         runCycleAnimation = new Timeline(imageKeyFrame);
+//         runCycleAnimation.setCycleCount(Timeline.INDEFINITE);
+//         
+//         animation.setOnFinished(e -> runCycleAnimation.stop());
+//         
+//         animation.play();
+//         runCycleAnimation.play();
+//         
+
+     }
 
     @FXML
     public void initialize() {
@@ -64,7 +130,17 @@ import javafx.stage.Stage;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+//        try {
+//            images[0] = new Image("file:images//runner1.png");
+//            images[1] = new Image("file:images//runner2.png");
+//            
+//            if (runnerMoving != null) {
+//                startX = runnerMoving.getLayoutX();
+//                runnerMoving.setImage(images[0]);
+//            }
+//        } catch (Exception e) {
+//            System.out.println("error" + e.getMessage());
+//    }
     }
 }    
     
--- src/main/MarathonMain.java
+++ src/main/MarathonMain.java
@@ -15,7 +15,8 @@ public class MarathonMain extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass()
-                .getResource("/View/marathon.fxml"));
+                .getResource("/View/"
+                        + "marathon.fxml"));
         Scene scene = new Scene(loader.load());
         primaryStage.setTitle("Marathon Simulator");
         primaryStage.setScene(scene);
