baa6a069a4cd01fcde0a3d3fc0a869d88da090be
yasmineel7 <6298686@edu.vaniercollege.qc.ca>
2025-11-12 13:54:27 -0500

    the getter, setter, equals and hashCode of the runner class

--- src/Model/Runner.java
+++ src/Model/Runner.java
@@ -4,13 +4,89 @@
  */
 package model;
 
+import java.util.Objects;
+
 public class Runner {
     // TODO: Define attributes (e.g., name, speed, distance)
     // TODO: Add constructors, getters, and setters
+    private String name;
+    private double distance;
+    private int number;
+    private double speed;
 
-    public Runner() {
-        // TODO: Initialize runner properties
+    public Runner(String name, double distance, int number, double speed) {
+        this.name = name;
+        this.distance = distance;
+        this.number = number;
+        this.speed = speed;
     }
 
     // TODO: Add methods (e.g., update distance, reset, etc.)
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 71 * hash + Objects.hashCode(this.name);
+        hash = 71 * hash + (int) (Double.doubleToLongBits(this.distance) ^ (Double.doubleToLongBits(this.distance) >>> 32));
+        hash = 71 * hash + this.number;
+        hash = 71 * hash + (int) (Double.doubleToLongBits(this.speed) ^ (Double.doubleToLongBits(this.speed) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Runner other = (Runner) obj;
+        if (Double.doubleToLongBits(this.distance) != Double.doubleToLongBits(other.distance)) {
+            return false;
+        }
+        if (this.number != other.number) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.speed) != Double.doubleToLongBits(other.speed)) {
+            return false;
+        }
+        return Objects.equals(this.name, other.name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+    
 }
