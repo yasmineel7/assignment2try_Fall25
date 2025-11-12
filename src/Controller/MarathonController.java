2727002126e5bedc19b326ecbaa2de89544f4bb6
yasmineel7 <6298686@edu.vaniercollege.qc.ca>
2025-11-12 14:20:32 -0500

    the fxml variable of the marathonController

--- src/Controller/MarathonController.java
+++ src/Controller/MarathonController.java
@@ -12,7 +12,10 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 /**
@@ -22,17 +25,35 @@ import javafx.stage.Stage;
  */
     public class MarathonController implements Initializable {
 
-   @FXML
-    private Label runnerLabel;
+  @FXML
+    private Label distanceLabel;
 
     @FXML
-    private Label distanceLabel;
+    private Pane firstLine;
+
+    @FXML
+    private Line fourthLine;
+
+    @FXML
+    private Rectangle lineRectangle;
 
     @FXML
     private Button playPauseButton;
 
     @FXML
     private Button resetButton;
+
+    @FXML
+    private Label runnerLabel;
+
+    @FXML
+    private Line secondLine;
+
+    @FXML
+    private Rectangle startRectangle;
+
+    @FXML
+    private Line thirdLine;
 
     @FXML
     public void initialize() {
