28b33b8e32b9168345e07b50dd2ce033ab9b1643
yasmineel7 <6298686@edu.vaniercollege.qc.ca>
2025-11-15 13:23:31 -0500

    added and continued some method about the speed and race

--- src/Model/modelRunner.java
+++ src/Model/modelRunner.java
@@ -16,51 +16,73 @@ public class modelRunner {
     private boolean raceStarted;
     private boolean raceFinished;
     private Runner winner;
+    private double raceDistance = 500.0;
+    private Runner selectedRunner;
 
-    public modelRunner(List<Runner> runners) {
-        this.runners = runners;
+    public modelRunner() {
+        this.runners = new ArrayList();
         this.raceStarted = false;
         this.raceFinished = false;
+        this.selectedRunner = runners.get(0);
+        initializeRunners();
     }
     
 
-  public void initializeRunner() {
+    public void initializeRunners() {
       
-  }
+    }
 
-  public void  updateRace() {
-      if (raceStarted && !raceFinished){
-          for (Runner runner : runners) {
+    public void  updateRace() {
+        if (raceStarted && !raceFinished){
+            for (Runner runner : runners) {
               runner.updatePosition();
-              if (runner.getPosition()>= 400 && winner == null) {
+                if (runner.getPosition()>= raceDistance && winner == null) {
                   winner = runner;
                   raceFinished = true;
                   runner.setIsWinner(true);
-              }
-          }
-      }
-  }
+                }
+            }
+        }
+    }
+    
+    public void increaseSpeedRunner() {
+        
+    }
+    
+    public void decreaseRunnerSpeed() {
+        
+    }
+    
+    /**
+     * rest the speed of the runner
+     */
+    public void resetAllSpeed() {
+        for (Runner runner: runners) {
+            runner.resetSpeed();
+        }
+    }
   
-  public void startRace() {
+    public void startRace() {
       raceStarted = true;
       raceFinished = false;
       winner = null;
        
-  }
+    }
   
-  public void pauseRace() {
+    public void pauseRace() {
       raceStarted = false;
-  }
+    }
   
-  public void resetRunner() {
+    public void resetRace() {
        for (Runner runner : runners) {
             runner.setPosition(0);
             runner.setWinner(false);
+            resetAllSpeed();
         }
         raceStarted = false;
         raceFinished = false;
         winner = null;
-  }
+    }
 
     public boolean isRaceFinished() {
         return raceFinished;
@@ -70,6 +92,7 @@ public class modelRunner {
         this.raceFinished = raceFinished;
     }
 
+    //getter and setter
     public List<Runner> getRunners() {
         return runners;
     }
